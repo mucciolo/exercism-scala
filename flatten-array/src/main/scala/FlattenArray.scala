0627c90752e4fe3ee539a1ba49149d6e238f37cd
mucciolo <diego_mucciolo@hotmail.com>
2022-04-03 21:28:48 -0300

    added type parameter to flatten

--- flatten-array/src/main/scala/FlattenArray.scala
+++ flatten-array/src/main/scala/FlattenArray.scala
@@ -2,14 +2,14 @@ import scala.annotation.tailrec
 
 object FlattenArray {
 
-  def flatten(list: List[Any]): List[Any] = {
+  def flatten[A](list: List[Any]): List[A] = {
 
     @tailrec
-    def iterate(list: List[Any], acc: List[Any] = List.empty): List[Any] = list match {
+    def iterate(list: List[Any], acc: List[A] = List.empty): List[A] = list match {
       case Nil => acc.reverse
       case x :: xs => x match {
-        case y: Int => iterate(xs, y :: acc)
         case ys: List[_] => iterate(ys ::: xs, acc)
+        case y: A => iterate(xs, y :: acc)
         case null => iterate(xs, acc)
       }
     }
